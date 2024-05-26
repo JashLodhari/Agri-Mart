0d55394a7c5a74263afc5609ab71d1533c1b1b31
JashLodhari <lodharijash024@gmail.com>
2024-05-26 16:26:19 +0530

    done!!!

--- lib/admin/add_vegetables.dart
+++ lib/admin/add_vegetables.dart
@@ -79,21 +79,39 @@ class _AddVegesState extends State<AddVeges> {
       ),
       body: SingleChildScrollView(
         child: Container(
-            margin:
-            EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0, bottom: 50.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
+          margin:
+          EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0, bottom: 50.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
               Text(
-              "Upload the Item Picture",
-              style: AppWidget.semiBoldTextFieldStyle(),
-            ),
-            SizedBox(height: 20.0),
-            selectedImage == null ? GestureDetector(
-              onTap: () {
-                getImage();
-              },
-              child: Center(
+                "Upload the Item Picture",
+                style: AppWidget.semiBoldTextFieldStyle(),
+              ),
+              SizedBox(height: 20.0),
+              selectedImage == null ? GestureDetector(
+                onTap: () {
+                  getImage();
+                },
+                child: Center(
+                  child: Material(
+                    elevation: 4.0,
+                    borderRadius: BorderRadius.circular(20),
+                    child: Container(
+                      width: 150,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black, width: 1.5),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Icon(
+                        Icons.camera_alt_outlined,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+              ) : Center(
                 child: Material(
                   elevation: 4.0,
                   borderRadius: BorderRadius.circular(20),
@@ -104,181 +122,163 @@ class _AddVegesState extends State<AddVeges> {
                       border: Border.all(color: Colors.black, width: 1.5),
                       borderRadius: BorderRadius.circular(20),
                     ),
-                    child: Icon(
-                      Icons.camera_alt_outlined,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.file(
+                        selectedImage!,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30.0),
+              Text(
+                "Item Name",
+                style: AppWidget.semiBoldTextFieldStyle(),
+              ),
+              SizedBox(height: 10.0),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                width: MediaQuery
+                    .of(context)
+                    .size
+                    .width,
+                decoration: BoxDecoration(
+                  color: Color(0xFFececf8),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextField(
+                  controller: namecontroller,
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Enter Item Name",
+                      hintStyle: AppWidget.LightTextFieldStyle()),
+                ),
+              ),
+              SizedBox(height: 30.0),
+              Text(
+                "Item Price",
+                style: AppWidget.semiBoldTextFieldStyle(),
+              ),
+              SizedBox(height: 10.0),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                width: MediaQuery
+                    .of(context)
+                    .size
+                    .width,
+                decoration: BoxDecoration(
+                  color: Color(0xFFececf8),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextField(
+                  controller: pricecontroller,
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Enter Item Price",
+                      hintStyle: AppWidget.LightTextFieldStyle()),
+                ),
+              ),
+              SizedBox(height: 30.0),
+              Text(
+                "Item Detail",
+                style: AppWidget.semiBoldTextFieldStyle(),
+              ),
+              SizedBox(height: 10.0),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                width: MediaQuery
+                    .of(context)
+                    .size
+                    .width,
+                decoration: BoxDecoration(
+                  color: Color(0xFFececf8),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextField(
+                  maxLines: 6,
+                  controller: detailcontroller,
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Enter Item Detail",
+                      hintStyle: AppWidget.LightTextFieldStyle()),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                "Select Category",
+                style: AppWidget.semiBoldTextFieldStyle(),
+              ),
+              SizedBox(height: 20.0),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10.0),
+                width: MediaQuery
+                    .of(context)
+                    .size
+                    .width,
+                decoration: BoxDecoration(
+                    color: Color(0xFFececf8),
+                    borderRadius: BorderRadius.circular(10)),
+                child: DropdownButtonHideUnderline(
+                  child: DropdownButton<String>(
+                    items: vegesitems
+                        .map((item) =>
+                        DropdownMenuItem<String>(
+                            value: item,
+                            child: Text(
+                              item,
+                              style: TextStyle(
+                                  fontSize: 18.0, color: Colors.black),
+                            )))
+                        .toList(),
+                    onChanged: ((value) =>
+                        setState(() {
+                          this.value = value;
+                        })),
+                    dropdownColor: Colors.white,
+                    hint: Text("Select Category"),
+                    iconSize: 36,
+                    icon: Icon(
+                      Icons.arrow_drop_down,
                       color: Colors.black,
                     ),
+                    value: value,
                   ),
                 ),
               ),
-            ) : Center(
-              child: Material(
-                elevation: 4.0,
-                borderRadius: BorderRadius.circular(20),
-                child: Container(
-                  width: 150,
-                  height: 150,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black, width: 1.5),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: Image.file(
-                      selectedImage!,
-                      fit: BoxFit.cover,
+              SizedBox(height: 30.0),
+              GestureDetector(
+                onTap: () {
+                  uploadItem();
+                },
+                child: Center(
+                  child: Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.circular(10),
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 5.0),
+                      width: 150,
+                      decoration: BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Center(
+                        child: Text(
+                          "Add",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 22.0,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ),
-            SizedBox(height: 30.0),
-        Text(
-          "Item Name",
-          style: AppWidget.semiBoldTextFieldStyle(),
-        ),
-        SizedBox(height: 10.0),
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 20.0),
-          width: MediaQuery
-              .of(context)
-              .size
-              .width,
-          decoration: BoxDecoration(
-            color: Color(0xFFececf8),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: TextField(
-            controller: namecontroller,
-            decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: "Enter Item Name",
-                hintStyle: AppWidget.LightTextFieldStyle()),
+              )
+            ],
           ),
         ),
-        SizedBox(height: 30.0),
-        Text(
-          "Item Price",
-          style: AppWidget.semiBoldTextFieldStyle(),
-        ),
-        SizedBox(height: 10.0),
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 20.0),
-          width: MediaQuery
-              .of(context)
-              .size
-              .width,
-          decoration: BoxDecoration(
-            color: Color(0xFFececf8),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: TextField(
-            controller: pricecontroller,
-            decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: "Enter Item Price",
-                hintStyle: AppWidget.LightTextFieldStyle()),
-          ),
-        ),
-        SizedBox(height: 30.0),
-        Text(
-          "Item Detail",
-          style: AppWidget.semiBoldTextFieldStyle(),
-        ),
-        SizedBox(height: 10.0),
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 20.0),
-          width: MediaQuery
-              .of(context)
-              .size
-              .width,
-          decoration: BoxDecoration(
-            color: Color(0xFFececf8),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: TextField(
-            maxLines: 6,
-            controller: detailcontroller,
-            decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: "Enter Item Detail",
-                hintStyle: AppWidget.LightTextFieldStyle()),
-          ),
-        ),
-        SizedBox(height: 20.0),
-        Text(
-          "Select Category",
-          style: AppWidget.semiBoldTextFieldStyle(),
-        ),
-        SizedBox(height: 20.0),
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 10.0),
-          width: MediaQuery
-              .of(context)
-              .size
-              .width,
-          decoration: BoxDecoration(
-              color: Color(0xFFececf8),
-              borderRadius: BorderRadius.circular(10)),
-          child: DropdownButtonHideUnderline(
-            child: DropdownButton<String>(
-              items: vegesitems
-                  .map((item) =>
-                  DropdownMenuItem<String>(
-                      value: item,
-                      child: Text(
-                        item,
-                        style: TextStyle(
-                            fontSize: 18.0, color: Colors.black),
-                      )))
-                  .toList(),
-              onChanged: ((value) =>
-                  setState(() {
-                    this.value = value;
-                  })),
-              dropdownColor: Colors.white,
-              hint: Text("Select Category"),
-              iconSize: 36,
-              icon: Icon(
-                Icons.arrow_drop_down,
-                color: Colors.black,
-              ),
-              value: value,
-            ),
-          ),
-        ),
-        SizedBox(height: 30.0),
-        GestureDetector(
-          onTap: (){
-            uploadItem();
-          },
-          child: Center(
-            child: Material(
-              elevation: 5.0,
-              borderRadius: BorderRadius.circular(10),
-              child: Container(
-                padding: EdgeInsets.symmetric(vertical: 5.0),
-                width: 150,
-                decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(10)),
-                child: Center(
-                  child: Text(
-                    "Add",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 22.0,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        )
-        ],
       ),
-    ),)
-    ,
     );
   }
 }
